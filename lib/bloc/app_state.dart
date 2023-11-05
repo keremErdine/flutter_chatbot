3b7c48431236f4e1756bdb145464d49d90e1fd3e
keremErdine <keremerdine@hotmail.com>
2023-11-05 13:49:09 +0300

    Removed the unused ai context variable

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,31 +1,24 @@
 part of 'app_bloc.dart';
 
 class AppState {
-  AppState(
-      {required this.aiContext,
-      required this.messages,
-      required this.documents});
-  final String aiContext;
+  AppState({required this.messages, required this.documents});
+
   final List<Message> messages;
   final List<lang_chain.Document> documents;
 
   factory AppState.initial() {
-    return AppState(aiContext: "", messages: [], documents: []);
+    return AppState(messages: [], documents: []);
   }
 
   AppState copyWith(
-      {String? aiContext,
-      List<Message>? messages,
-      List<lang_chain.Document>? documents}) {
+      {List<Message>? messages, List<lang_chain.Document>? documents}) {
     return AppState(
-        aiContext: aiContext ?? this.aiContext,
         messages: messages ?? this.messages,
         documents: documents ?? this.documents);
   }
 
   AppState addMessage(Message message) {
     List<Message> list = [message];
-    return AppState(
-        aiContext: aiContext, messages: messages + list, documents: documents);
+    return AppState(messages: messages + list, documents: documents);
   }
 }
