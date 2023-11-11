4569a7f039005e7199be14191334348a83942a15
keremErdine <keremerdine@hotmail.com>
2023-11-11 19:14:51 +0300

    e

--- lib/ai/ai_toolkit.dart
+++ lib/ai/ai_toolkit.dart
@@ -1,1 +1,14 @@
-enum BookType { history, science }
+//import 'package:langchain/langchain.dart';
+
+//enum BookType { history, science }
+
+///This function is used to read books of different topics.
+///If the user wants to learn info about any topic in "İnkilap Tarihi ve Atatürkçülük" lesson use [BookType.history]
+///If the user wants to learn info about any topic in "Fen Bilimleri" lesson use [BookType.science]
+/*
+Future<String> readBook(BookType bookType) async {
+  if (bookType == BookType.history) {
+    return await TextLoader(filePath).load();
+  }
+}
+*/
