dbaa7806281a29c18cbc224ebc0fec7859a7dc0f
keremErdine <keremerdine@hotmail.com>
2023-11-12 15:27:38 +0300

    Created the drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -9,8 +9,67 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(child: BlocBuilder<AppBloc, AppState>(
       builder: (context, state) {
-        return const Column(
-          children: [],
+        return Column(
+          children: [
+            Row(
+              children: [
+                const Icon(Icons.smart_toy_outlined),
+                Text(
+                  "Hocam Bot",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            //Sohbet Ekranı
+            if (state.screen == Screen.mainScreen)
+              Row(
+                children: [
+                  const Icon(Icons.smart_toy_outlined),
+                  Text("Sohbet", style: Theme.of(context).textTheme.titleLarge),
+                ],
+              )
+            else
+              TextButton.icon(
+                onPressed: () {
+                  context
+                      .read<AppBloc>()
+                      .add(AppScreenChanged(screen: Screen.mainScreen));
+                },
+                icon: const Icon(Icons.smart_toy_outlined),
+                label: Text(
+                  "Sohbet",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            const SizedBox(
+              height: 5,
+            ),
+            //Hakkında Ekranı
+            if (state.screen == Screen.aboutScreen)
+              Row(
+                children: [
+                  const Icon(Icons.menu_book_outlined),
+                  Text("Hakkında",
+                      style: Theme.of(context).textTheme.titleLarge),
+                ],
+              )
+            else
+              TextButton.icon(
+                onPressed: () {
+                  context
+                      .read<AppBloc>()
+                      .add(AppScreenChanged(screen: Screen.aboutScreen));
+                },
+                icon: const Icon(Icons.menu_book_outlined),
+                label: Text(
+                  "Hakkında",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              )
+          ],
         );
       },
     ));
