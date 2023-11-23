1fdf6cf10333dda85a5369a4c627189776127241
keremErdine <keremerdine@hotmail.com>
2023-11-23 12:38:08 +0300

    Changed the welcome screen a bit

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_chatbot/bloc/app_bloc.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Column(
-            children: [
-              Text(
-                "Hocam Bot",
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                "Öğretici yapay zeka uygulaması",
-                style: Theme.of(context).textTheme.displaySmall,
-              )
-            ],
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            "Hocam Bot",
+            style: Theme.of(context).textTheme.displayMedium,
           ),
-        )
-      ],
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            "Öğretici yapay zeka uygulaması",
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          const Spacer(),
+          ElevatedButton(
+              onPressed: () {
+                context
+                    .read<AppBloc>()
+                    .add(AppScreenChanged(screen: Screen.chatScreen));
+              },
+              child: Text(
+                "Şimdi Başla",
+                style: Theme.of(context).textTheme.headlineLarge,
+              )),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -9,94 +9,128 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(child: BlocBuilder<AppBloc, AppState>(
       builder: (context, state) {
-        return ListView(
-          children: [
+        return ListView(children: [
+          Row(
+            children: [
+              const Icon(Icons.smart_toy_outlined),
+              Text(
+                "Hocam Bot",
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.left,
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          //Sohbet Ekranı
+          if (state.screen == Screen.chatScreen)
             Row(
               children: [
+                const SizedBox(
+                  width: 12,
+                ),
                 const Icon(Icons.smart_toy_outlined),
                 Text(
-                  "Hocam Bot",
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  "Sohbet",
+                  style: Theme.of(context).textTheme.titleLarge,
                   textAlign: TextAlign.left,
-                )
+                ),
+              ],
+            )
+          else
+            Row(
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    context
+                        .read<AppBloc>()
+                        .add(AppScreenChanged(screen: Screen.chatScreen));
+                  },
+                  icon: const Icon(Icons.smart_toy_outlined),
+                  label: Text(
+                    "Sohbet",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const Spacer(),
               ],
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            //Sohbet Ekranı
-            if (state.screen == Screen.chatScreen)
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  const Icon(Icons.smart_toy_outlined),
-                  Text(
-                    "Sohbet",
-                    style: Theme.of(context).textTheme.titleLarge,
-                    textAlign: TextAlign.left,
-                  ),
-                ],
-              )
-            else
-              Row(
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      context
-                          .read<AppBloc>()
-                          .add(AppScreenChanged(screen: Screen.chatScreen));
-                    },
-                    icon: const Icon(Icons.smart_toy_outlined),
-                    label: Text(
-                      "Sohbet",
-                      style: Theme.of(context).textTheme.titleMedium,
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  const Spacer(),
-                ],
-              ),
-            const SizedBox(
-              height: 5,
-            ),
-            //Hakkında Ekranı
-            if (state.screen == Screen.aboutScreen)
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  const Icon(Icons.menu_book_outlined),
-                  Text(
+          const SizedBox(
+            height: 5,
+          ),
+          //Hakkında Ekranı
+          if (state.screen == Screen.aboutScreen)
+            Row(
+              children: [
+                const SizedBox(
+                  width: 12,
+                ),
+                const Icon(Icons.menu_book_outlined),
+                Text(
+                  "Hakkında",
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            )
+          else
+            Row(
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    context
+                        .read<AppBloc>()
+                        .add(AppScreenChanged(screen: Screen.aboutScreen));
+                  },
+                  icon: const Icon(Icons.menu_book_outlined),
+                  label: Text(
                     "Hakkında",
-                    style: Theme.of(context).textTheme.titleLarge,
+                    style: Theme.of(context).textTheme.titleMedium,
                     textAlign: TextAlign.left,
                   ),
-                ],
-              )
-            else
-              Row(
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      context
-                          .read<AppBloc>()
-                          .add(AppScreenChanged(screen: Screen.aboutScreen));
-                    },
-                    icon: const Icon(Icons.menu_book_outlined),
-                    label: Text(
-                      "Hakkında",
-                      style: Theme.of(context).textTheme.titleMedium,
-                      textAlign: TextAlign.left,
-                    ),
+                ),
+                const Spacer(),
+              ],
+            ),
+          if (state.screen == Screen.welcomeScreen)
+            Row(
+              children: [
+                const SizedBox(
+                  width: 12,
+                ),
+                const Icon(Icons.smart_toy_outlined),
+                Text(
+                  "Başlangıç",
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            )
+          else
+            Row(
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    context
+                        .read<AppBloc>()
+                        .add(AppScreenChanged(screen: Screen.welcomeScreen));
+                  },
+                  icon: const Icon(Icons.smart_toy_outlined),
+                  label: Text(
+                    "Başlangıç",
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.left,
                   ),
-                  const Spacer(),
-                ],
-              )
-          ],
-        );
+                ),
+                const Spacer(),
+              ],
+            ),
+          const SizedBox(
+            height: 5,
+          ),
+        ]);
       },
     ));
   }
